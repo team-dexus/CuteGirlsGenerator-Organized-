641e2106649976ac44bd5101115040c19f2aae82
JPNYKW <31243896+JPNYKW@users.noreply.github.com>
2018-08-01 10:51:14 +0900

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -1,30 +1,27 @@
-//created by JPNYKW
-
-var wid=128,hig=128; // for draw engine
-
-CanvasRenderingContext2D.prototype.drawImageByData=function($Array,$width,$height,$find,$px,$x,$y){
-	let $dy=$y+$px/2;
-	let $idx=$find;
-	for($hc=0;$hc<$height;$hc++){
-		let $dx=$x+$px/2;
-		for($wc=0;$wc<$width;$wc++){
-			let R=$Array[$idx + ($Array.length / 3) * 0]*127.5+127.5;
-			let G=$Array[$idx + ($Array.length / 3) * 1]*127.5+127.5;
-			let B=$Array[$idx + ($Array.length / 3) * 2]*127.5+127.5;
-			R=~~R+',';G=~~G+',';B=~~B+'';
-			this.drawFillBox($dx,$dy,$px+1,`rgb(${R+G+B})`);
-			$dx+=$px;
-			$idx+=1;
+wid = 128, hig = 128; // for draw engine
+CanvasRenderingContext2D.prototype.drawImageByData = function(ary, width, height, find, px, x, y) {
+	let dy = y + px / 2;
+	let idx = find;
+	
+	for(let hc = 0; hc < height; hc++) {
+		let dx = x + px / 2;
+		for(let wc = 0; wc < width; wc++) {
+			let R = ary[idx + (ary.length / 3) * 0] * 127.5 + 127.5;
+			let G = ary[idx + (ary.length / 3) * 1] * 127.5 + 127.5;
+			let B = ary[idx + (ary.length / 3) * 2] * 127.5 + 127.5;
+			this.drawFillBox(dx, dy, px + 1,`rgb({~~R},{~~G},{~~B},)`);
+			dx += px;
+			idx++;
 		}
-		$dy+=$px;
+		dy += px;
 	}
-	// debugger fpr console
-	c.timeEnd();
-	c.log('%cDebug','color:#0093EF;font-weight:bold',`id:${$idx}`);
-	c.log('%cDebug %cSuccessful','color:#0093EF;font-weight:bold','color:#F53300;font-weight:bold','draw an Image');
+	// debug
+	console.timeEnd();
+	console.log('%cDebug', 'color:#0093EF;font-weight:bold', `id:{idx}`);
+	console.log('%cDebug %cSuccessful', 'color:#0093EF;font-weight:bold', 'color:#F53300;font-weight:bold', 'draw an Image');
 }
 
-function drawGeneratedImage(){
-      cont.clearRect(0,0,canv.width,canv.height);
-      cont.drawImageByData(generatedData,wid,hig,0,2,0,0);
+function drawGeneratedImage() {
+      cont.clearRect(0, 0, canv.width, canv.height);
+      cont.drawImageByData(generatedData, wid, hig, 0, 2, 0, 0);
 }
