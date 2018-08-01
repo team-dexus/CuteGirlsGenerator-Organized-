bf087b6189ce18fb37fbbaeef8728ec40f619e6d
JPNYKW <31243896+JPNYKW@users.noreply.github.com>
2018-08-01 10:29:55 +0900

    Update generate.js
    コードの整形、エラー処理追加

--- js/generate.js
+++ js/generate.js
@@ -1,64 +1,61 @@
+function generate() {
+	let pic = document.getElementById('GirlPic'),
+	item = ['MG7.png', 'MG57.png', 'code100.png'],
+	text = document.getElementById('words').value,
+	arrayRes = document.getElementById('ArrayResult');
+	//result = '===WORDS===<br>';
 
-function generate(){ //Added by mirukuma ,Changed by JPNYKW ,Changed by mirukuma ,Changed by JPNYKW
-  var pic=d.getElementById('GirlPic');
-  var item=['MG7.png','MG57.png','code100.png']
-  var text=d.getElementById('words').value;
-  var arrayRes=d.getElementById('ArrayResult');
-  var result='===WORDS===<br>';
-  tag(text).forEach(function(e,i,a){result+=`<span id=res${i}>${e}</span><br>`});
-  d.getElementById('ShowText').textContent='Generation result in '+text;
-  d.getElementById('ArrayRes').innerHTML=`${result}=========`;
-  pic.src=item[~~(Math.random()*3)];
-
-
+	tag(text).forEach((e, i) => result += `<span id=res${i}>${e}</span><br>`);
+	document.getElementById('ShowText').textContent = `Generation result in ${text}`;
+	//document.getElementById('ArrayRes').innerHTML = `${result}=========`;
+	pic.src = item[~~(Math.random() * 3)];
 }
 
-function generate2(){
-  d.getElementById("generate").setAttribute("disabled", true)
-  if (onGenerated) d.getElementById("download").setAttribute("disabled", true)
-  onGenerated=false; //Added by JPNYKW
+function generate2() {
+	document.getElementById('generate').setAttribute('disabled', true);
+	onGenerated && document.getElementById('download').setAttribute('disabled', true);
+	onGenerated = false;
 
-  //生成
-  let x = runner.inputs[0];
-  let y = runner.outputs[0];
+	// Generate
+	let x = runner.inputs[0], y = runner.outputs[0], noise;
 
-  noise_size = 128;
-  var noise= new Float32Array(noise_size)
+	noise_size = 128;
+	noise = new Float32Array(noise_size);
+	for(let i = 0; i < noise_size; i++) {
+		noise[i] = Math.random() * 2 - 1;
+	}
 
-  for(i=0; i<noise_size; i=i+1){
-    noise[i]=Math.random()*2-1
-  }
+/*
+	numberOfTag = 1539;
+	var tags = new Float32Array(numberOfTag);
+	for(let i = 0; i < 4; i++){
+		tags[Math.floor(Math.random() * 1539) - 1] = 1;
+	}
+/*
+	for(let i = 0; i < 1539; i++){
+		tags[i] = Math.random();
+	}
+*/
 
-  /*
-  numberOfTag=1539
+	// Select the tag
+	x.set(noise);
+	console.log(x);
+	console.log('sex'); // :ha:
 
-  var tags= new Float32Array(numberOfTag)
+	runner.run().then(function() {
+		if (y_typed_array.includes(NaN)) {
+			alert('Failed the generated.');
+		} else {
+			console.log('finished');
+			let y_typed_array = y.toActual();
+			console.log(y_typed_array);
 
-
-  for(i=0; i<4; i=i+1){
-    tags[Math.floor(Math.random()*1539)-1]=1
-  }
-  /*
-  for(i=0; i<1539; i=i+1){
-    tags[i]=Math.random()
-  }
-  */
-
-  //タグの指定
-
-  x.set(noise);
-  console.log(x)
-  console.log("sex")
-  runner.run()
-    .then(function() {
-      console.log('finished');
-      let y_typed_array = y.toActual();
-      console.log(y_typed_array);
-      generatedData=y_typed_array; //Added by JPNYKW
-      drawGeneratedImage(); //Added by JPNYKW
-      onGenerated=true; //Added by JPNYKW
-      d.getElementById("generate").removeAttribute("disabled")
-      d.getElementById("download").removeAttribute("disabled")
-
+			generatedData = y_typed_array;
+			drawGeneratedImage();
+		}
+		
+		onGenerated = true;
+		document.getElementById('generate').removeAttribute('disabled');
+		document.getElementById('download').removeAttribute('disabled');
     });
 }
