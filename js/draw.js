632401c8a342c63b98843a7ca2b74d99bbd4f8b0
JPNYKW <31243896+JPNYKW@users.noreply.github.com>
2018-08-01 11:07:22 +0900

    Update draw.js
    ばぐしゅせ

--- js/draw.js
+++ js/draw.js
@@ -1,15 +1,16 @@
 wid = 128, hig = 128; // for draw engine
 CanvasRenderingContext2D.prototype.drawImageByData = function(ary, width, height, find, px, x, y) {
-	let dy = y + px / 2;
-	let idx = find;
+	let z = ary.length / 3,
+		dy = y + px / 2,
+		idx = find;
 	
 	for(let hc = 0; hc < height; hc++) {
 		let dx = x + px / 2;
 		for(let wc = 0; wc < width; wc++) {
-			let R = ary[idx + (ary.length / 3) * 0] * 127.5 + 127.5;
-			let G = ary[idx + (ary.length / 3) * 1] * 127.5 + 127.5;
-			let B = ary[idx + (ary.length / 3) * 2] * 127.5 + 127.5;
-			this.drawFillBox(dx, dy, px + 1,`rgb({~~R},{~~G},{~~B},)`);
+			let R = ary[idx + z * 0] * 127.5 + 127.5;
+			let G = ary[idx + z * 1] * 127.5 + 127.5;
+			let B = ary[idx + z * 2] * 127.5 + 127.5;
+			this.drawFillBox(dx, dy, px + 1,`rgb(${~~R},${~~G},${~~B})`);
 			dx += px;
 			idx++;
 		}
